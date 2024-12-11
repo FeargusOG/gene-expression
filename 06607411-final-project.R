fab1c9f742d9c6766a1a19612bc61eb6878b1034
Feargus O'Gorman <feargusog@gmail.com>
2024-12-11 16:58:38 +0000

    enrichment wip

--- 06607411-final-project.R
+++ 06607411-final-project.R
@@ -43,7 +43,7 @@ data_cna <- data_cna[, common_cols]
 ## Extract the "ERBB2" row from the CNA dataset and filter it so that we just
 ## have the patient data.
 erbb2_cna <- data_cna[data_cna$Hugo_Symbol == "ERBB2", ] |>
-  select(-Hugo_Symbol, -Entrez_Gene_Id)
+  dplyr::select(-Hugo_Symbol, -Entrez_Gene_Id)
 rownames(erbb2_cna) <- "ERBB2"
 
 ## Print the counts of each ERBB2 value.
@@ -62,7 +62,7 @@ metadata <- matrix(0, ncol = 1, nrow = ncol(assay))
 for (i in seq_len(ncol(assay))) {
   ## Find the patient with the matching ID in the CNA ERB22 dataset.
   patient_col <- erbb2_cna |> 
-    select(colnames(assay)[i])
+    dplyr::select(colnames(assay)[i])
   ## Check the ERBB2 amplification level (greater than 0 is amplified)
   metadata[i, 1] <- ifelse(as.numeric(patient_col[1,1]) > 0, 1, 0)
 }
@@ -85,7 +85,9 @@ assay <- assay[keep, ]
 
 ## Ensure BiocManager and DESeq2 are installed
 ensure_package("BiocManager")
+ensure_package("BiocParallel") # DESeq2 was slow! Let's use more cores....
 ensure_package("DESeq2")
+library(BiocParallel)
 library(DESeq2)
 
 ## Create the differential expression analysis dataset.
@@ -104,7 +106,10 @@ dds <- DESeqDataSetFromMatrix(countData = assay,
                               design = ~ ERBB2_Amp)
 
 ## Run DESeq2 differential expression analysis
-dds <- DESeq(dds)
+## Use one less core than the total, but ensure at least 1 worker
+workers <- max(1, parallel::detectCores() - 2)
+register(MulticoreParam(workers = workers))
+dds <- DESeq(dds, parallel = TRUE)
 res <- results(dds)
 
 # 9. Obtain Differentially Expressed Genes
@@ -113,8 +118,128 @@ top_genes <- res[order(res$padj)[1:10], ]
 print(top_genes)
 
 # 10. Perform a Pathway Enrichment Analysis
+ensure_package("clusterProfiler")
+library(clusterProfiler)
+ensure_package("org.Hs.eg.db")
+library(org.Hs.eg.db)
+ensure_package("enrichplot")
+library(enrichplot)
+
+## TODO Why this number?? p-value?
+res_sig = res[res$padj<0.05,]
+DE_over = rownames(res_sig[res_sig$log2FoldChange>0,])
+DE_under = rownames(res_sig[res_sig$log2FoldChange<0,])
+
 ## Gene Ontology (GO) enrichment analysis
+### Find genes that are over-expressed in ERBB2 amplified patients.
+go_results_over = enrichGO(
+  gene          = DE_over,
+  OrgDb         = org.Hs.eg.db,
+  keyType       = "SYMBOL",
+  ont           = "BP", # TODO why not use default MF
+  pAdjustMethod = "BH", # TODO This is the default... can remove.
+  pvalueCutoff  = 0.05, # TODO This is the default... can remove.
+  qvalueCutoff  = 0.05 # TODO Why not use default 0.2?
+)
+print(head(go_results_over))
+
+### Find genes that are under-expressed in ERBB2 amplified patients.
+go_results_under = enrichGO(
+  gene          = DE_under,
+  OrgDb         = org.Hs.eg.db,
+  keyType       = "SYMBOL",  
+  ont           = "BP", # TODO why not use default MF
+  pAdjustMethod = "BH", # TODO This is the default... can remove.
+  pvalueCutoff  = 0.05, # TODO This is the default... can remove.
+  qvalueCutoff  = 0.05 # TODO Why not use default 0.2?
+)
+
+print(head(go_results_under))
+
+### Plot results of GO enrichment analysis
+dotplot(go_results_over, showCategory=10) + 
+  ggtitle("Gene Ontology Enrichment Over Expressed")
+
+dotplot(go_results_under, showCategory=10) + 
+  ggtitle("Gene Ontology Enrichment Under Expressed")
+
 ## KEGG Pathway Enrichment Analysis
+### Map symbol to entrez for Reactome and Keggs
+gene_entrez_over <- bitr(
+  DE_over,
+  fromType = "SYMBOL",
+  toType   = "ENTREZID",
+  OrgDb    = org.Hs.eg.db
+)
+
+gene_entrez_under <- bitr(
+  DE_under,
+  fromType = "SYMBOL",
+  toType   = "ENTREZID",
+  OrgDb    = org.Hs.eg.db
+)
+
+kegg_results_over =  enrichKEGG(
+  gene          = gene_entrez_over[,2],
+  organism      = "human",   
+  pAdjustMethod = "BH",
+  pvalueCutoff  = 0.05,
+  qvalueCutoff  = 0.05
+)
+
+kegg_results_under =  enrichKEGG(
+  gene          = gene_entrez_under[,2],
+  organism      = "human",   
+  pAdjustMethod = "BH",
+  pvalueCutoff  = 0.05,
+  qvalueCutoff  = 0.05
+)
+
+print(head(kegg_results_over))
+print(head(kegg_results_under))
+
+### Plot results of KEGG pathway enrichment analysis
+dotplot(kegg_results_over, showCategory=10) + 
+  ggtitle("Kegg Pathway Enrichment Over Expressed")
+dotplot(kegg_results_under, showCategory=10) + 
+  ggtitle("Kegg Pathway Enrichment Under Expressed")
+
+## Reactome Pathway Enrichment Analysis
+ensure_package("ReactomePA")
+library(ReactomePA)
+ensure_package("pathview")
+library(pathview)
+
+reactome_results_over =  enrichPathway(
+  gene          = gene_entrez_over[,2],
+  organism      = "human",   
+  pAdjustMethod = "BH",
+  pvalueCutoff  = 0.05,
+  qvalueCutoff  = 0.05,
+)
+
+reactome_results_under =  enrichPathway(
+  gene          = gene_entrez_under[,2],
+  organism      = "human",   
+  pAdjustMethod = "BH",
+  pvalueCutoff  = 0.05,
+  qvalueCutoff  = 0.05,
+)
+
+print(head(reactome_results_over))
+print(head(reactome_results_under))
+
+### Plot results of Reactome pathway enrichment analysis
+dotplot(reactome_results_over, showCategory=10) + 
+  ggtitle("Reactome Pathway Enrichment Over Expressed")
+dotplot(reactome_results_under, showCategory=10) + 
+  ggtitle("Reactome Pathway Enrichment Under Expressed")
+
+go_results_under_pw = pairwise_termsim(go_results_under)
+treeplot(go_results_under_pw)+ ggtitle("GO Enrichment Under Expressed")
+
+kegg_results_under_pw = pairwise_termsim(kegg_results_under)
+treeplot(kegg_results_under_pw)+ ggtitle("KEGG Enrichment Under Expressed")
 
 # 11. Get the variance stabilised transformed expression values.
 vsd <- vst(dds)
